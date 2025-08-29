9e919ed28b1d9c10c76a9fbbf46f9b90c2deb8df
maaretp <maaret.pyhajarvi@gmail.com>
2025-08-29 08:58:55 +0300

    End of session

--- test_eprime.py
+++ test_eprime.py
@@ -1,6 +1,7 @@
 import requests
 import approvaltests
 from playwright.sync_api import Page
+import pytest
 
 URL = "https://qe-at-cgi-fi.github.io/eprime/"
 
@@ -9,18 +10,33 @@ def test_request():
     assert response.status_code == 200
     approvaltests.verify_html(response.text)
 
-def test_playwright(page: Page):
+def this_is_sample():
+    with open("sample.txt") as f:
+        lines = f.readlines()
+    return str(lines)
+
+data = [
+    ("to be or not be", "5", "2", "0"), 
+    ("maaret's", 1, 0, 1), 
+    ("to be " *1000, 2000, 1000, 0), 
+    (this_is_sample(), 3, 0, 0)
+    #bug: too many words
+    #("to be or not to be - hamlet's dilemma", 8, 2, 1)
+]
+
+
+@pytest.mark.parametrize("input_text, expect_1, expect_2, expect_3", data)
+def test_playwright(page: Page, input_text, expect_1, expect_2, expect_3):
     page.goto(URL)
+    page.fill("#test_field", input_text)
+    page.click("#test_button")
+    assert page.inner_text("#test_number_1") == str(expect_1)
+    assert page.inner_text("#test_number_2") == str(expect_2)
+    assert page.inner_text("#test_number_3") == str(expect_3)
     
 
+
 # SESSION
-# Requests or Playwright?
-# webgui testing dependencies and setup after git clone
-# pytest.ini for command line parameters
-# Stepwise Script
-# Fix the test target application
-# Parametrize
-# Parametrize with test name
 # Input from file
 
 # SESSION
