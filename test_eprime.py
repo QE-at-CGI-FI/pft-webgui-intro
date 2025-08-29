fe41e6b4440715da97ebf00585f24aca6434850a
maaretp <maaret.pyhajarvi@gmail.com>
2025-08-29 08:11:51 +0300

    Add instructions

---
+++ test_eprime.py
@@ -0,0 +1,18 @@
+URL = "https://qe-at-cgi-fi.github.io/eprime/"
+
+# SESSION
+# Commit the instructions before starting steps
+# Requests or Playwright?
+# webgui testing dependencies and setup after git clone
+# pytest.ini for command line parameters
+# Stepwise Script
+# Fix the test target application
+# Parametrize
+# Parametrize with test name
+# Input from file
+
+# SESSION
+# Fixtures for webgui in conftest.py
+# Refactor to classes and page objects
+# Containerize
+# Run in GitHub Actions
